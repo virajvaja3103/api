5c6a444cc922b39a5f8840b3786975bf87da2353
viraj vaja <313viraj@gmail.com>
2022-04-08 17:02:22 +0530

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,8 +19,14 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'stock'=>$this->stock==0?'out of stock':$this->stock,
+            'discount'=>$this->discount,
+            'total_price'=>round((1-($this->discount/100))*$this->discount,2),
+             'rating'=> $this->reviews->count()>0? round($this->reviews->sum('star')/$this->reviews->count(),2)
+             :'no rating yet',
+            'href'=>[
+                'reviews'=> route('reviews.index',$this->id)
+            ]
         ];
     }
 }
