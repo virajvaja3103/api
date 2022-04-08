261dfb82988708e0ad9dd1ead02faf5c64d63a71
viraj vaja <313viraj@gmail.com>
2022-04-08 15:43:16 +0530

    relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Review;
 
 class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable  = ['name','discount','stock','text','price'];
+    // protected $fillable  = ['name','discount','stock','text','price'];
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Model\Product;
 
 class Review extends Model
 {
     use HasFactory;
 
-    protected $fillable  = ['customer','review','star'];
+    // protected $fillable  = ['customer','review','star'];
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
