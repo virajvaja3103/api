7343bd90ac1fbcd9643cc7a81aa99c8ed59e6501
viraj vaja <313viraj@gmail.com>
2022-04-08 10:51:19 +0530

    created Model -a

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/product', ProductController::class);
+
+Route::group(['prefix'=>'product'],function(){
+
+    Route::apiResource('/{product}/reviews',ReviewController::class);
+
 });
