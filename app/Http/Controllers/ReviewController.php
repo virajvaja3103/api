23dbb8d0fff36f1e8382eccf8e5ce0aa132cc4d4
viraj vaja <313viraj@gmail.com>
2022-04-12 12:04:46 +0530

    review update and delete

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -76,9 +76,12 @@ class ReviewController extends Controller
      * @param  \App\Models\Model\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateReviewRequest $request, Review $review)
+    public function update(UpdateReviewRequest $request, Product $product,Review $review)
     {
-        //
+        $review->update($request->all());
+        return response([
+            "data"=> new ReviewResource($review)
+        ],Response::HTTP_CREATED);
     }
 
     /**
@@ -87,8 +90,9 @@ class ReviewController extends Controller
      * @param  \App\Models\Model\Review  $review
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Review $review)
+    public function destroy(Product $product,Review $review)
     {
-        //
+        $review->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
